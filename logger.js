883005e951eeb1032bd1fad57d1fd57a36db247d
ReaggyXX <125679271+ReaggyXX@users.noreply.github.com>
2025-03-04 22:27:27 +0000

    Add files via upload

---
+++ logger.js
@@ -0,0 +1,9 @@
+class Logger {
+    static log(message) {
+        console.log(`[LOG] ${new Date().toISOString()}: ${message}`);
+    }
+
+    static error(message) {
+        console.error(`[ERROR] ${new Date().toISOString()}: ${message}`);
+    }
+} 
